4fa8fd2bd883ed81d1ddaf14f4408d06df4a62d2
LingFenglong <2808021998@qq.com>
2023-09-25 19:40:49 +0800

    all

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/lingfenglong/springcloud/controller/OrderController.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/lingfenglong/springcloud/controller/OrderController.java
@@ -1,16 +1,24 @@
 package com.lingfenglong.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 @RequestMapping("/consumer")
+@RefreshScope
 public class OrderController {
     private final String PAYMENT_URL = "http://cloudalibaba-provider-payment";
     private final RestTemplate restTemplate;
+
+    @Value("${lingfenglong.config}")
+    private String config;
 
     @Autowired
     public OrderController(RestTemplate restTemplate) {
@@ -19,6 +27,7 @@ public class OrderController {
 
     @GetMapping("/payment/info")
     public String info() {
+        log.info(config);
         return restTemplate.getForObject(PAYMENT_URL + "/payment/info", String.class);
     }
 }
